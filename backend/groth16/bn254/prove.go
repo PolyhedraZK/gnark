457b9142ae786915d09fe1ceae69728ea87277ce
liyue201 <liyue201@126.com>
2025-07-15 13:56:34 +0800

    remove log

--- backend/groth16/bn254/prove.go
+++ backend/groth16/bn254/prove.go
@@ -320,7 +320,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 
 func serialProve(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...backend.ProverOption) (*Proof, error) {
 
-	fmt.Printf("serialProve\n")
+	//fmt.Printf("serialProve\n")
 
 	opt, err := backend.NewProverConfig(opts...)
 	if err != nil {
